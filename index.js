b4bdab2af8b21994f3f981931a766b1b4144ef69
Nils Tekampe <nt@konfidas.de>
2019-04-07 12:56:14 +0000

    yargs

--- index.js
+++ index.js
@@ -6,6 +6,19 @@ const fs = require('fs');
 
 const files = require('./lib/files');
 
+
+var argv = require('yargs')
+    .usage('Usage: $0 <command> [options]')
+    .command('count', 'Count the lines in a file')
+    .example('$0 count -f foo.js', 'count the lines in the given file')
+    .alias('o', 'output')
+    .nargs('o', 1)
+    .describe('o', 'Save output to this file')
+    .help('h')
+    .alias('h', 'help')
+    .epilog('copyright 2019')
+    .argv;
+
 clear();
 console.log(
     chalk.yellow(
@@ -13,39 +26,31 @@ console.log(
     )
 );
 
-var minimist = require('minimist')
 
-var args = minimist(process.argv.slice(2), {
-    alias: { h: 'help', v: 'version', o: 'output' },
-    stopEarly: true, /* populate _ with first non-option */
-    unknown: function() {
-        console.log("nicht gut")
 
-    } /* invoked on unknown param */
-})
-
-var numberOfArguments = args["_"].length;
+var numberOfArguments = argv["_"].length;
 
 if (numberOfArguments != 2) {
     console.log("Please provide exactly two arguments that stand for the html files to be compared.")
     return;
 }
 
-var htmlFile1 = args["_"][0];
-var htmlFile2 = args["_"][1];
+
+var htmlFile1 = argv['_'][0];
+var htmlFile2 = argv['_'][1];
 
 
 if (fs.existsSync(htmlFile1) == false) {
-    console.log(htmlFile1 + " nicht gefunden")
+   console.log(htmlFile1 + " nicht gefunden")
 }
 
 if (fs.existsSync(htmlFile2) == false) {
-    console.log(htmlFile2 + " nicht gefunden")
+   console.log(htmlFile2 + " nicht gefunden")
 }
 
-console.dir(args);
-console.log(args["_"][1]);
-console.log(args["_"].length);
+//console.dir(args);
+//console.log(args["_"][1]);
+//console.log(args["_"].length);
 
 
 
