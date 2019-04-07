d46f30575742709de40e7279c60cbdc5d2449402
Nils Tekampe <nt@konfidas.de>
2019-04-07 07:32:04 +0000

    Zwischenstand

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const chalk = require('chalk');
 const clear = require('clear');
-const figlet= require('figlet');
+const figlet = require('figlet');
 const fs = require('fs');
 
 
@@ -8,9 +8,9 @@ const files = require('./lib/files');
 
 clear();
 console.log(
-  chalk.yellow(
-    figlet.textSync('htmldiff', { horizontalLayout: 'full' })
-  )
+    chalk.yellow(
+        figlet.textSync('htmldiff', { horizontalLayout: 'full' })
+    )
 );
 
 var minimist = require('minimist')
@@ -18,38 +18,33 @@ var minimist = require('minimist')
 var args = minimist(process.argv.slice(2), {
     alias: { h: 'help', v: 'version', o: 'output' },
     stopEarly: true, /* populate _ with first non-option */
-    unknown: function () { 
+    unknown: function() {
         console.log("nicht gut")
 
     } /* invoked on unknown param */
-  })
+})
 
-
-
-var numberOfArguments=args["_"].length;
+var numberOfArguments = args["_"].length;
 
 if (numberOfArguments != 2) {
-
-  console.log("Please provide exactly two arguments that stand for the html files to be compared.")
-  return;
-
+    console.log("Please provide exactly two arguments that stand for the html files to be compared.")
+    return;
 }
 
-var htmlFile1=args["_"][0];
-var htmlFile2=args["_"][1];
+var htmlFile1 = args["_"][0];
+var htmlFile2 = args["_"][1];
 
 
 if (fs.existsSync(htmlFile1) == false) {
-  console.log(htmlFile1 + " nicht gefunden")
-
+    console.log(htmlFile1 + " nicht gefunden")
 }
 
+if (fs.existsSync(htmlFile2) == false) {
+    console.log(htmlFile2 + " nicht gefunden")
+}
 
-// var argv = require('minimist')(process.argv.slice(2));
 console.dir(args);
-
 console.log(args["_"][1]);
-
 console.log(args["_"].length);
 
 
